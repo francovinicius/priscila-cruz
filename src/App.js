646792dd1fa2547ef66a864a74e807332686c5fb
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-20 14:37:50 -0300

    Feito o roteamento de componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Banner from './Components/Banner'
+import About from './Components/About'
+import Links from './Components/Links'
+import Form from './Components/Form'
+import Footer from "./Components/Rodape";
 
 function App() {
   return (
     <BrowserRouter>
+
+    <Banner />
+
       <Routes>
-        <Route path="/" element={<Banner />} />
+        <Route path="/" element={<About />} />
+        <Route path="/links" element={<Links />} />
+        <Route path="/contato" element={<Form />} />
       </Routes>
+
+    <Footer />
     </BrowserRouter>
   );
 }
--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -5,9 +5,9 @@ export default function Banner() {
         <section>
             <div className="d-flex flex-column align-items-center justify-content-center">
                 <div className='d-flex align-items-center justify-content-center navegacao'>
-                    <a className="nav-link" href="#about">Sobre</a>
-                    <a className="nav-link" href="#links">Links</a>
-                    <a className="nav-link" href="#form">Contato</a>
+                    <a className="nav-link" href="/">Sobre</a>
+                    <a className="nav-link" href="/links">Links</a>
+                    <a className="nav-link" href="/contato">Contato</a>
                 </div>
 
                 <img className="img-fluid" src="./assets/img/priscila-1.svg" alt="Priscila" />
