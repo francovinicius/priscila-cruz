2399de8c42cb2ce6163e5ccc254d49a64855fede
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-22 23:41:04 -0300

    alteração do sobre

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -5,10 +5,16 @@ export default function About() {
         <section>
 
             <div className='d-flex flex-column justify-content-center align-items-center' id='about'>
-                <p className='p-apresentacao'>
-                    Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui 
-                    Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui 
-                    Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui Escreva um resumo sobre você aqui.
+                <p className='p-apresentacao text-center'>
+                    Priscila é uma cantora, compositora e pastora com mais de 15 anos de experiência em liderança de adoração. Ela tem formação no Seminário Médio Teológico e, junto com seu esposo Franklin Cruz, lidera a Local 7 Church em Volta Redonda - RJ. Além de suas responsabilidades ministeriais, ela também é mãe do Davi e da Mel.
+                    <br />
+                    Priscila tem uma forte conexão com Deus e seu amor pela Palavra se reflete em suas canções. Ela acredita que sua música é mais do que apenas entretenimento, mas uma forma de compartilhar a mensagem do amor de Deus, a revelação de Jesus e a manifestação do Espírito Santo.
+                    <br />
+                    Sua liderança espiritual, combinada com sua habilidade musical, permite que ela transmita sua mensagem de uma maneira única e inspiradora. Como mãe, Priscila enfrenta desafios diários, mas sua fé e confiança em Deus a ajudam a equilibrar suas responsabilidades familiares e ministeriais com graça e sabedoria.
+
+
+
+
                 </p>
             </div>
 
