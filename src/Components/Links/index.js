66c0247644c028cd100a1da4eb7121fcae28e529
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-03-26 11:48:13 -0300

    alteração na escrita

--- src/Components/Links/index.js
+++ src/Components/Links/index.js
@@ -17,7 +17,7 @@ export default function Links() {
                     <a className="nav-link" aria-current="page" target='_blank' href="https://m.facebook.com/100045620257794/">Facebook</a>
                 </div>
                 <div className='div-link d-flex flex-column align-items-center justify-content-center'>
-                    <a className="nav-link" aria-current="page" target='_blank' href="https://open.spotify.com/artist/4yUtuKurjPDoWupy1t3pys">Spotfy</a>
+                    <a className="nav-link" aria-current="page" target='_blank' href="https://open.spotify.com/artist/4yUtuKurjPDoWupy1t3pys">Spotify</a>
                 </div>
                 <div className='div-link d-flex flex-column align-items-center justify-content-center'>
                     <a className="nav-link" aria-current="page" target='_blank' href="https://www.deezer.com/br/artist/5619940?deferredFl=1&utm_campaign=artist&utm_source=google&utm_medium=organic">Deezer</a>
