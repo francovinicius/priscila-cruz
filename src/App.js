36f2cd707ee38946766ebbf7c4bf654e87fae49c
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-11 13:42:54 -0300

    alt atualização card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Banner from './Components/Banner'
-import About from './Components/About'
 import Links from './Components/Links'
 import Form from './Components/Form'
 import Footer from "./Components/Rodape";
@@ -12,8 +11,7 @@ function App() {
     <Banner />
 
       <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/links" element={<Links />} />
+        <Route path="/" element={<Links />} />
         <Route path="/contato" element={<Form />} />
       </Routes>
 
--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -5,13 +5,12 @@ export default function Banner() {
         <section>
             <div className="d-flex flex-column align-items-center justify-content-center">
                 <div className='d-flex align-items-center justify-content-center navegacao'>
-                    <a className="nav-link" href="/">Sobre</a>
                     <a className="nav-link" href="/links">Links</a>
                     <a className="nav-link" href="/contato">Contato</a>
                 </div>
 
-                <img className="img-fluid" src="./assets/img/priscila-2.svg" alt="Priscila" />
-                <h3>@priscilacruz_</h3>
+                <img className="img-fluid" src="./assets/img/priscila-bg.svg" alt="Priscila" />
+                <h3>Priscila Cruz</h3>
 
 
             </div>
